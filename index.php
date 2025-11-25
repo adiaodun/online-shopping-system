348bdbd1fd337676be2fb86c79e46029f6932a94
adiaodun <adiaobong.odungide@convergroup.com>
2025-11-25 16:16:23 +0100

    Remove OpenTelemetry tracing from index.php
    Removed OpenTelemetry tracing setup and spans from index.php.

--- index.php
+++ index.php
@@ -1,32 +1,9 @@
 <?php
-require 'vendor/autoload.php';
 
-use OpenTelemetry\API\Trace\TracerProvider;
-use OpenTelemetry\SDK\Trace\TracerProviderFactory;
 
-// Initialize tracer provider
-$tracerProvider = (new TracerProviderFactory())->create();
-$tracer = $tracerProvider->getTracer('lab-online-app');
+include "header.php";
 
-// Start a span for the incoming request
-$requestSpan = $tracer->spanBuilder('incoming-request')->startSpan();
-
-try {
-    // Include your application sections once
-    include "header.php";
-
-    // Optional: wrap body in its own span
-    $bodySpan = $tracer->spanBuilder('render-body')->startSpan();
-    include "body.php";
-    $bodySpan->end();
-
-    include "newslettter.php";
-    include "footer.php";
-
-} catch (Exception $e) {
-    $requestSpan->recordException($e);
-}
-
-$requestSpan->end();
+include "body.php";
+include "newslettter.php";
+include "footer.php";
 ?>
-		
